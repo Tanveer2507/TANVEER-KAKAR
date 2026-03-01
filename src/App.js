16bca0e1bda33ed4dfe20c02fae2be7e9d0ddcef
Tanveer2507 <tanveerkakar294@gmail.com>
2026-03-01 15:32:16 +0530

    Remove Resume section from website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
@@ -28,7 +27,6 @@ function App() {
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Home />
       <About />
-      <Resume />
       <Projects />
       <Skills />
       <Contact />
